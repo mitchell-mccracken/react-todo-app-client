d0c016dcd399bb04942b87b26e59303255d43f69
Mitch McCracken <mitchell@conturoprototyping.com>
2024-05-06 15:54:00 -0400

    Updated to display the userName if token exists in the cookies

--- src/homePage.jsx
+++ src/homePage.jsx
@@ -1,18 +1,49 @@
 // import { useState } from 'react';
 import NavBar from './navBar';
+import axios from 'axios';
+import { CookiesProvider, useCookies } from 'react-cookie';
+import { setURL } from './utils';
+import { useState } from 'react';
 
 
 function HomePage() {
-  // let count = 0;
-  // const [ count, setCount ] = useState(0);
 
-  // const func = () => { setCount(count + 1) };
+
+  // check if a cookie exists
+  const [cookies, setCookie, removeCookie] = useCookies();
+  const [userName, setUserName] = useState(null);
+
+
+  // fetch userName based on token value
+  const fetchUserName = () => {
+
+    const token = cookies.mitch_test_app_token;
+
+    if ( token ) {
+      const data = { token };
+      axios.post ( setURL('/api/user/fetchUserName'), data )
+      .then( res => {
+
+        const { userName } = res.data;
+        if ( userName ) {
+          setUserName( userName );
+        }
+
+      } )
+      .catch( e => console.error( e ) )
+    }
+  };
+  fetchUserName();
 
   return (
     <div>
       {/* <NavBar /> */}
       <NavBar />
       <h1>home page</h1>
+      <h3>
+        { (cookies.mitch_test_app_token) ? userName : 'NOT LOGGED IN'}
+      </h3>
+
     </div>
   );
 }
