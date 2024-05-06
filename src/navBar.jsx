21ead085d4db5f1c519d18ccfefadd30563c8a43
Mitch McCracken <mitchell@conturoprototyping.com>
2024-05-06 16:11:55 -0400

    Restructured to split links so that login is on the right side of the screen

--- src/navBar.jsx
+++ src/navBar.jsx
@@ -1,28 +1,28 @@
 
 import { Link } from 'react-router-dom';
 import './styles/navBar.css';
-import HomePage from './homePage';
-import LoginPage from './loginPage';
-import TodoPage from './todoPage';
 
 
 function NavBar() {
-
-  const navToCounterPage = () => { window.open('./mitchPage') };
 
 
 
   return (
     <div id='main'>
       <div id="nav-bar">
-        <div>
-          <Link to="/">Home</Link>
+        <div id="links-wrapper">
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/todo">Todo List</Link>
+          </div>
         </div>
-        <div>
-          <Link to="/todo">Todo List</Link>
-        </div>
-        <div>
-          <Link to="/login">Login Page</Link>
+
+        <div id="user-info-wrapper">
+          <div>
+            <Link to="/login">Login</Link>
+          </div>
         </div>
       </div>
     </div>
