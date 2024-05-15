d754d05f35794e41dc7a8779c0f98ef9668fa19a
Mitch McCracken <mitchell@conturoprototyping.com>
2024-05-15 12:34:25 -0400

    Added payment funtion

--- src/components/NewOrderComponent.jsx
+++ src/components/NewOrderComponent.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useState } from 'react';
 import { HandleInputChange } from '../utils';
 import { CreditCard, PaymentForm } from "react-square-web-payments-sdk";
+import axios from 'axios';
+import { setURL } from '../utils';
 
 const NewOrderComponent = ( { show } ) => {
 
@@ -15,6 +17,18 @@ const NewOrderComponent = ( { show } ) => {
   const handleInputChange = HandleInputChange( setNewOrder );
 
   const logValues = () => { console.log( newOrder ) };
+
+  const paymentFunction = ( token, verifiedBuyer ) => {
+    console.log('token:', token);
+    console.log('verifiedBuyer:', verifiedBuyer);
+
+    axios.post( setURL('/api/user/payment'), { sourceId: token.token } )
+    .then( res => {
+      console.log(res.data)
+    })
+    .catch( e => console.error( e ) );
+
+  };
 
   return (show) ? 
   (
@@ -56,12 +70,19 @@ const NewOrderComponent = ( { show } ) => {
             placeholder='card number'
           />
         </div>
-        <PaymentForm
+        {/* <PaymentForm
           applicationId="sandbox-sq0idb-BO9q6ECgkcitNE4rJpOQiw"
           cardTokenizeResponseReceived={async (token, verifiedBuyer) => {
             console.log('token:', token);
             console.log('verifiedBuyer:', verifiedBuyer);
           }}
+          locationId='XXXXXXXXXX'
+        >
+          < CreditCard />
+        </PaymentForm> */}
+        <PaymentForm
+          applicationId="sandbox-sq0idb-BO9q6ECgkcitNE4rJpOQiw"
+          cardTokenizeResponseReceived={paymentFunction}
           locationId='XXXXXXXXXX'
         >
           < CreditCard />
